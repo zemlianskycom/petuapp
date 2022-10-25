6b40c8b83e441640e0992c18e32c4f6a7c1cb2ea
Pavel S <sampashass@gmail.com>
2022-10-25 12:12:23 +0700

    +jwtToken

--- back/controllers/user/regController.js
+++ back/controllers/user/regController.js
@@ -1,5 +1,6 @@
 import User from '../../models/userModel.js'
 import asyncHandler from 'express-async-handler'
+import { generateToken } from '../../helpers/generateToken.js'
 
 // @desc Register user
 // @route POST /api/users
@@ -14,14 +15,12 @@ export const registerUser = asyncHandler(async (req, res) => {
     throw new Error('Такой пользователь уже зарегистрирован')
   }
 
-  throw new Error('Test')
-
   const user = await User.create({
     email,
     password,
   })
 
-  //Create token
+  const token = generateToken(user._id)
 
-  res.json(user)
+  res.json({ user, token })
 })
